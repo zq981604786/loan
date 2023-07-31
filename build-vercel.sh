31faeac4a780337d569b6de06cf7140eaf5c18e6
zhaoquan <zhaoquan@1token.trade>
2023-07-31 22:07:18 +0800

    feat:update sh

--- build-vercel.sh
+++ build-vercel.sh
@@ -12,11 +12,3 @@ cp -r dist/. ./
 # 添加 vercel.json 配置文件
 echo '{"builds": [{"src": "dist/index.html","use": "@vercel/static"}]}' > vercel.json
 
-# 初始化Git仓库
-git init
-git add .
-git commit -m "Deploy new version"
-
-# 推送到GitHub仓库触发Vercel部署
-git remote add origin https://github.com/<user>/<repo>
-git push -u origin master
